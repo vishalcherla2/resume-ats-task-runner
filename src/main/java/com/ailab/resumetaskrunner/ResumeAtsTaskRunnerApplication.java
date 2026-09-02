9aca2cdcda68a725a1f14387d3fe77df2211a381
Vishal <cherlavishal2@gmail.com>
2026-09-02 09:35:16 +0530

    Initialized Spring Boot project and MySQL configuration

---
+++ src/main/java/com/ailab/resumetaskrunner/ResumeAtsTaskRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.ailab.resumetaskrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAtsTaskRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAtsTaskRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ailab/resumetaskrunner/ResumeAtsTaskRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailab.resumetaskrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAtsTaskRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
